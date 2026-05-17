0e14a84e9f951dcc18cf3a505e8fc32dd39175f2
Dougwithanoug <hobbesnonumbers@gmail.com>
2026-05-17 00:03:42 -0400

    bullet types change based on enemy (#10)

--- scene/bullet.gd
+++ scene/bullet.gd
@@ -3,17 +3,43 @@ extends Area2D
 var speed = 20
 enum type {PLAYER, ENEMY_1, ENEMY_2} #to be set on call by level
 var bullet_type = type 
+var bullet_direction = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$despawn_timer.start()
+	$bullet_large_hitbox.disabled=true
+	$bullet_small_hitbox.disabled=true
+	#$bullet_sprite.hide()
 	rotation = PI/2
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	position += Vector2(sin(rotation),-cos(rotation)).normalized()*speed*bullet_direction
+	
 func _process(delta: float) -> void:
-	#if bullet_type==type.PLAYER:
-	position += Vector2(sin(rotation),-cos(rotation)).normalized()*speed
 	pass
 
 func _on_despawn_timer_timeout() -> void:
 	queue_free()
+
+func set_bullet_type(new_type) -> void:
+	if new_type == "ENEMY_1":
+		bullet_type = type.ENEMY_1
+		$bullet_small_hitbox.disabled=false
+		$bullet_sprite.play("enemy_1")
+		$bullet_sprite.show()
+		bullet_direction = -1
+		
+	elif new_type == "ENEMY_2":
+		bullet_type = type.ENEMY_2
+		$bullet_large_hitbox.disabled=false
+		$bullet_sprite.play("enemy_2")
+		$bullet_sprite.show()
+		bullet_direction = -1
+		
+	else:
+		bullet_type = type.PLAYER
+		$bullet_small_hitbox.disabled=false
+		$bullet_sprite.play("player")
+		$bullet_sprite.show()
+		bullet_direction = 1
--- src/enemy_ship_node.gd
+++ src/enemy_ship_node.gd
@@ -4,28 +4,22 @@ extends Area2D
 signal die
 signal fire
 var health: int
-var health_gun = 1
-var health_shield =2
 var health_enemy_small = 1
 var health_enemy_big = 2
-var speed_player = 10
 var speed_enemy = 10
 var speed =1 #placeholder speed
 var target_direction: Vector2
 var target_location: Vector2
-enum type  {GUN, SHIELD, ENEMY_SMALL, ENEMY_BIG}
-var ship_type = type
-var player_z = 11
+enum EnemyType  {ENEMY_SMALL, ENEMY_BIG}
+var ship_type = EnemyType
 var enemy_z =12
 
 
 
 func _ready() -> void:
 	$sprite.hide()
-	$hitbox_gun.hide()
-	$hitbox_shield.hide()
-	$hitbox_enemy_sm.hide()
-	$hitbox_enemy_bg.hide()
+	$hitbox_enemy_sm.disabled = true
+	$hitbox_enemy_bg.disabled = true
 	target_location=position
 	
 func _physics_process(delta: float) -> void:
@@ -37,45 +31,26 @@ func _process(delta: float) -> void:
 	pass
 
 
-func spawn_shield() -> void: #spawns ship as shield ship, sets animation, health, hitbox
-	$sprite.play("shield")
-	$sprite.show()
-	$hitbox_shield.show()
-	$bullet_timer.start()
-	z_index=player_z
-	speed = speed_player
-	health = health_shield
-	ship_type = type.SHIELD
-
-func spawn_gun() -> void: #spawns ship as gun ship, sets animation, health, hitbox
-	$sprite.play("gun")
-	$sprite.show()
-	$hitbox_gun.show()
-	$bullet_timer.start()
-	z_index=player_z
-	speed = speed_player
-	health = health_gun
-	ship_type = type.GUN
 
 func spawn_enemy_small() -> void: #spawns ship as gun ship, sets animation, health, hitbox
 	$sprite.play("enemy_small")
 	$sprite.show()
-	$hitbox_enemy_sm.show()
+	$hitbox_enemy_sm.disabled = false
 	$bullet_timer.start()
 	z_index=enemy_z
 	speed = speed_enemy
 	health = health_enemy_small
-	ship_type = type.ENEMY_SMALL
+	ship_type = EnemyType.ENEMY_SMALL
 
 func spawn_enemy_big() -> void: #spawns ship as gun ship, sets animation, health, hitbox
 	$sprite.play("enemy_big")
 	$sprite.show()
-	$hitbox_enemy_bg.show()
+	$hitbox_enemy_bg.disabled = false
 	$bullet_timer.start()
 	z_index=enemy_z
 	speed = speed_enemy
 	health = health_enemy_big
-	ship_type = type.ENEMY_BIG
+	ship_type = EnemyType.ENEMY_BIG
 
 func flyto(location: Vector2) -> void: #recieves a target position as vector2
 	target_location = location
@@ -87,14 +62,7 @@ func flyto(location: Vector2) -> void: #recieves a target position as vector2
 
 func _unhandled_input(event: InputEvent) -> void:  #placeholder for spawning ships, to be removed/disabled
 	if event is InputEventKey:#placeholder for spawning ships, to be removed/disabled
-		if event.pressed and event.keycode == KEY_2:#placeholder for spawning ships, to be removed/disabled
-			spawn_shield()#placeholder for spawning ships, to be removed/disabled
-			print("key hit")
-			#position=Vector2(100,100)#placeholder for spawning ships, to be removed/disabled
-		elif event.pressed and event.keycode == KEY_1:#placeholder for spawning ships, to be removed/disabled
-			spawn_gun()#placeholder for spawning ships, to be removed/disabled
-			#position=Vector2(100,100)#placeholder for spawning ships, to be removed/disabled
-		elif event.pressed and event.keycode == KEY_3:#placeholder for spawning ships, to be removed/disabled
+		if event.pressed and event.keycode == KEY_3:#placeholder for spawning ships, to be removed/disabled
 			var randpoint = Vector2(randi() % 1120+32, randi() % 616+32)#placeholder for spawning ships, to be removed/disabled
 			flyto(randpoint)#placeholder for spawning ships, to be removed/disabled
 			#print(randpoint)#placeholder for spawning ships, to be removed/disabled
@@ -113,9 +81,12 @@ func _on_area_entered(area: Area2D) -> void:
 
 
 func _on_timer_timeout() -> void:
-	if ship_type!=type.SHIELD:
-		var bullet = bullet_scene.instantiate()
-		bullet.position = position 
-		get_tree().get_root().add_child(bullet)
+	var bullet = bullet_scene.instantiate()
+	bullet.position = position
+	if ship_type == EnemyType.ENEMY_SMALL:
+		bullet.set_bullet_type("ENEMY_1")
+	elif ship_type == EnemyType.ENEMY_BIG:
+		bullet.set_bullet_type("ENEMY_2")
+	get_tree().get_root().add_child(bullet)
 		
 	
