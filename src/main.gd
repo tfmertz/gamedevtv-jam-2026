f3733f40bb945d68961b45369f83234e4cb1d679
Tom Mertz <tomfmertz@gmail.com>
2026-05-16 20:05:22 -0700

    Add main transition and title

--- src/main.gd
+++ src/main.gd
@@ -1,11 +1,26 @@
-extends Node2D
+class_name GameManager extends Node2D
 
+@export var default_scene: PackedScene
+@onready var scene_root: Node2D = $SceneRoot
+@onready var color_rect: ColorRect = $Transitions/ColorRect
 
+var is_transitioning := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	scene_root.add_child(default_scene.instantiate())
+	fade_in()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func fade_out(duration = 1.0):
+	var tween = create_tween()
+	tween.tween_property(color_rect, "color:a", 1.0, duration)
+	await tween.finished
+
+func fade_in(duration = 1.0):
+	var tween = create_tween()
+	tween.tween_property(color_rect, "color:a", 0.0, duration)
+	await tween.finished
