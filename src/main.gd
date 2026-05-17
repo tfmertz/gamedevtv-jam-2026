e8ab0625e78911804a17cb267c28dc02d8b45cb2
Tom Mertz <tomfmertz@gmail.com>
2026-05-16 22:49:38 -0700

    Title and start

--- src/main.gd
+++ src/main.gd
@@ -1,4 +1,4 @@
-class_name GameManager extends Node2D
+extends Node2D
 
 @export var default_scene: PackedScene
 @onready var scene_root: Node2D = $SceneRoot
@@ -8,9 +8,8 @@ var is_transitioning := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	scene_root.add_child(default_scene.instantiate())
-	fade_in()
+	fade_in(3)
 	
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -24,3 +23,11 @@ func fade_in(duration = 1.0):
 	var tween = create_tween()
 	tween.tween_property(color_rect, "color:a", 0.0, duration)
 	await tween.finished
+
+func load_scene(scene_path):
+	await fade_out()
+	for child in scene_root.get_children():
+		child.queue_free()
+	var new_scene := load(scene_path)
+	scene_root.add_child(new_scene.instantiate())
+	await fade_in()
