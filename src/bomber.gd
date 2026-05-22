955b3478dd17115f91589a4f930be710d89127df
isaac h <ihad91@gmail.com>
2026-05-21 23:41:28 -0400

    adding ship-ship collision

--- src/bomber.gd
+++ src/bomber.gd
@@ -48,7 +48,6 @@ func _on_attack_timer_timeout() -> void:
 
 
 func _on_area_entered(area: Area2D) -> void:
-	print('hit')
 	# if we hit something that can take damage
 	if area is ShipNode:
 		# explode
