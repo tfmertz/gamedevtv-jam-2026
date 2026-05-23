648fac544f86fecdf4b8e5789babfbc9aa9b5acb
isaac h <ihad91@gmail.com>
2026-05-23 00:47:56 -0400

    typo

--- src/ship_connection_edge.gd
+++ src/ship_connection_edge.gd
@@ -5,7 +5,7 @@ class_name ShipConnectionEdge extends Area2D
 var source_ship : ShipNode = null
 var dest_ship   : ShipNode = null
 
-# Called when the node enters the ascene tree for the first time.
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	z_index = -1
 	
