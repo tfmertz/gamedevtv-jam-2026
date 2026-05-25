f899583eb51634ed1c934ad3c7f86fee47b75893
Tom Mertz <tomfmertz@gmail.com>
2026-05-25 11:31:09 -0700

    Fix game won music

--- src/boss_wave.gd
+++ src/boss_wave.gd
@@ -210,7 +210,6 @@ func _die() -> void:
 		enemy._die()
 	await get_tree().create_timer(3.0).timeout
 	GameManager.load_scene("res://scene/ui/game_won.tscn")
-	AudioManager.play_music(AudioManager.game_won_stream)
 	
 	'''
 func _unhandled_input(event: InputEvent) -> void:  #placeholder for spawning ships, to be removed/disabled
--- src/title.gd
+++ src/title.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+@export var play_won_music := false
+
+func _ready() -> void:
+	if play_won_music:
+		_play_game_won_music()
+
 func _on_button_press():
 	AudioManager.cross_fade_music(AudioManager._game_player)
 	GameManager.load_scene("res://scene/level.tscn")
@@ -8,3 +14,6 @@ func _on_retry_press():
 	GameManager.load_scene("res://scene/level.tscn")
 	if not AudioManager._game_player.playing:
 		AudioManager.cross_fade_music(AudioManager._game_player)
+
+func _play_game_won_music():
+	AudioManager.play_music(AudioManager.game_won_stream)
